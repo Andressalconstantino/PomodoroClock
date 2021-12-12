2ac5f2d608c8cb7c7b45891d5d587287a457af08
Andressalconstantino <86380516+Andressalconstantino@users.noreply.github.com>
2021-12-12 18:38:26 -0300

    new features

--- script.js
+++ script.js
@@ -62,20 +62,103 @@ const img5 = document.getElementById('fiveI');
 const img6 = document.getElementById('sixI');
 
 img1.addEventListener('click', function (){
-    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1223148.jpg')";
+    document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/e3/13/a0/e313a08e5b0455b1d2b5f345b2cdb97f.jpg')";
 });
 img2.addEventListener('click', function (){
-    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1223169.jpg')";
+    document.body.style.backgroundImage = "url('https://cutewallpaper.org/21/cozy-wallpapers/Cozy-Desktop-Wallpaper-Pinterest-Phone-Wallpapers.jpg')";
 });
+
 img3.addEventListener('click', function (){
-    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1223616.jpg')";
+    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/3335268.jpg')";
 });
 img4.addEventListener('click', function (){
-    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/6863016.png')";
+    document.body.style.backgroundImage = "url('https://wallpaperbat.com/img/536661-elysa-roberts-on-photography-art-desktop-wallpaper-macbook-computer-wallpaper-desktop-wallpaper-laptop-wallpaper-desktop-wallpaper.jpg')";
 });
 img5.addEventListener('click', function (){
     document.body.style.backgroundImage = "url('https://cdn.wallpapersafari.com/5/79/9kWJjU.jpg')";
 });
 img6.addEventListener('click', function (){
-    document.body.style.backgroundImage = "url('https://cutewallpaper.org/21/cozy-wallpaper-hd/A-Cozy-Night-To-Lament-On-Things-HD-Wallpaper-Background-.jpg')";
+    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/791.jpg')";
 });
+
+/* ---------------- */
+
+/* COUNTDOWN TIMERS */
+let end = false;
+function startTimer(duration, display, state) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        if(!state) {
+
+            document.getElementById('sb').addEventListener('click', function (){
+                timer = (60 * 5), minutes, seconds;
+            })
+            document.getElementById('lb').addEventListener('click', function (){
+                timer = (60 * 15), minutes, seconds;
+            })
+
+            document.getElementById('pause').addEventListener('click', function (){
+                state = true;
+                document.getElementById('image').src = "reload.png";
+                document.getElementById('continue').style.display = 'block'
+                document.getElementById('play').style.display = 'block'
+                document.getElementById('pause').style.display = 'none'
+                document.getElementById('play').style.margin = "13.5rem 0 0 4.5rem"
+            })
+            document.getElementById('continue').addEventListener('click', function (){
+                state = false;
+                document.getElementById('play').style.display = 'none'
+                document.getElementById('continue').style.display = 'none'
+                document.getElementById('pause').style.display = 'block'
+            })
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                state = true;
+                document.getElementById('image').src = "reload.png";
+                document.getElementById('play').style.display = 'block'
+                document.getElementById('pause').style.display = 'none'
+            }
+        }
+    }, 1000);
+}
+
+let state = false;
+document.getElementById('play').addEventListener('click', function (){
+    state = false;
+    let countMinutes = 60 * 25,
+        display = document.querySelector('#time');
+    startTimer(countMinutes, display, state);
+    document.getElementById('continue').style.display = 'none'
+    document.getElementById('play').style.display = 'none'
+    document.getElementById('pause').style.display = 'block'
+})
+
+
+/* ------------------------- */
+
+/* SOUND */
+
+document.getElementById('rain').addEventListener('click', function (){
+    document.getElementById('audio').src = "rain.mp3";
+})
+document.getElementById('crowd').addEventListener('click', function (){
+    document.getElementById('audio').src = "crowd.mp3";
+})
+document.getElementById('ocean').addEventListener('click', function (){
+    document.getElementById('audio').src = "ocean.mp3";
+})
+document.getElementById('streets').addEventListener('click', function (){
+    document.getElementById('audio').src = "streets.mp3";
+})
+document.getElementById('creepy').addEventListener('click', function (){
+    document.getElementById('audio').src = "creepy.mp3";
+})
+document.getElementById('none').addEventListener('click', function (){
+    document.getElementById('audio').src = "";
+})
